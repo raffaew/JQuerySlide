71e8e527ecfa60d955425a12a507df1db8c32b1e
Rafael Dias <92760236+raffaew@users.noreply.github.com>
2023-12-29 15:45:38 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,60 +1,77 @@
-$(function() {
-    $('#opt1').click(function() {
-        $('#img1').show();
-        $('#img2').hide();
-        $('#img3').hide();
-        $('#img4').hide();
-    })
+$(function () {
+  $("#opt1").click(function () {
+    $("#img1").show();
+    $("#img2").hide();
+    $("#img3").hide();
+    $("#img4").hide();
+  });
 
-    $('#opt2').click(function() {
-        $('#img1').hide();
-        $('#img2').show();
-        $('#img3').hide();
-        $('#img4').hide();
-    })
-    
-    $('#opt3').click(function() {
-        $('#img1').hide();
-        $('#img2').hide();
-        $('#img3').show();
-        $('#img4').hide();
-    })
-    
-    $('#opt4').click(function() {
-        $('#img1').hide();
-        $('#img2').hide();
-        $('#img3').hide();
-        $('#img4').show();
-    })
-    
-})
+  $("#opt2").click(function () {
+    $("#img1").hide();
+    $("#img2").show();
+    $("#img3").hide();
+    $("#img4").hide();
+  });
 
-$(function() {
-    $('#opt1').click(function() {
-        $('#mbl1').show();
-        $('#mbl2').hide();
-        $('#mbl3').hide();
-        $('#mbl4').hide();
-    })
+  $("#opt3").click(function () {
+    $("#img1").hide();
+    $("#img2").hide();
+    $("#img3").show();
+    $("#img4").hide();
+  });
 
-    $('#opt2').click(function() {
-        $('#mbl1').hide();
-        $('#mbl2').show();
-        $('#mbl3').hide();
-        $('#mbl4').hide();
-    })
-    
-    $('#opt3').click(function() {
-        $('#mbl1').hide();
-        $('#mbl2').hide();
-        $('#mbl3').show();
-        $('#mbl4').hide();
-    })
-    
-    $('#opt4').click(function() {
-        $('#mbl1').hide();
-        $('#mbl2').hide();
-        $('#mbl3').hide();
-        $('#mbl4').show();
-    })
-})
+  $("#opt4").click(function () {
+    $("#img1").hide();
+    $("#img2").hide();
+    $("#img3").hide();
+    $("#img4").show();
+  });
+});
+
+$(function () {
+  $("#opt1").click(function () {
+    $("#mbl1").show();
+    $("#mbl2").hide();
+    $("#mbl3").hide();
+    $("#mbl4").hide();
+  });
+
+  $("#opt2").click(function () {
+    $("#mbl1").hide();
+    $("#mbl2").show();
+    $("#mbl3").hide();
+    $("#mbl4").hide();
+  });
+
+  $("#opt3").click(function () {
+    $("#mbl1").hide();
+    $("#mbl2").hide();
+    $("#mbl3").show();
+    $("#mbl4").hide();
+  });
+
+  $("#opt4").click(function () {
+    $("#mbl1").hide();
+    $("#mbl2").hide();
+    $("#mbl3").hide();
+    $("#mbl4").show();
+  });
+});
+
+$(function () {
+  let isClick = false;
+
+  $(".menu").click(function () {
+    isClick = !isClick;
+
+    if (isClick) {
+      $(".logo").hide();
+      $(".links a").css({
+        display: "inline",
+        "font-size": "18px",
+      });
+    } else {
+      location.reload();
+    }
+  });
+});
