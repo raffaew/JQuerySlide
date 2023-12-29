1a650d5918c190d982359f7ce34ed6a5d82f20f8
Rafael Dias <92760236+raffaew@users.noreply.github.com>
2023-12-29 12:55:12 -0300

    testeMobile

--- js/script.js
+++ js/script.js
@@ -28,3 +28,33 @@ $(function() {
     })
     
 })
+
+$(function() {
+    $('#opt1').click(function() {
+        $('#mbl1').show();
+        $('#mbl2').hide();
+        $('#mbl3').hide();
+        $('#mbl4').hide();
+    })
+
+    $('#opt2').click(function() {
+        $('#mbl1').hide();
+        $('#mbl2').show();
+        $('#mbl3').hide();
+        $('#mbl4').hide();
+    })
+    
+    $('#opt3').click(function() {
+        $('#mbl1').hide();
+        $('#mbl2').hide();
+        $('#mbl3').show();
+        $('#mbl4').hide();
+    })
+    
+    $('#opt4').click(function() {
+        $('#mbl1').hide();
+        $('#mbl2').hide();
+        $('#mbl3').hide();
+        $('#mbl4').show();
+    })
+})
